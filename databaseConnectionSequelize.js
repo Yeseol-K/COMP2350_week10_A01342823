8d1edf00dda4334e4f428162d25fa3c678865cbd
unknown <ye37seol@gmailcom>
2024-03-22 15:43:44 -0700

    fix mysql connection

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,9 +1,13 @@
 const is_qoddi = process.env.IS_QODDI || false;
-const dbConfigQoddi = "mysql://freedb_2350_main.@sql.freedb.tech:3306/freedb_COMP2350_A01342823";
+
+const dbConfigQoddi = "mysql://freedb_2350_main.:&c!gQXSFgVweD*4@sql.freedb.tech:3306/freedb_COMP2350_A01342823";
+
 const dbConfigLocal = "mysql://root:abc123!@localhost/freedb_COMP2350_A01342823";
+
 if (is_qoddi) {
   var databaseConnectionString = dbConfigQoddi;
 } else {
   var databaseConnectionString = dbConfigLocal;
 }
+
 module.exports = databaseConnectionString;
